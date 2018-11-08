207c78786af7347db1228a90122e5bcd77d29e85
NicolsNeto <npneto@thoughtworks.com>
2018-11-08 15:12:45 -0200

    criado search e primeira funcao de busca

--- Search/search.js
+++ Search/search.js
@@ -1,11 +1,12 @@
 
 var board =
 [
-  ["A","M","O","R","R","O","M","A","F"],
+
   ["A","V","E","G","R","I","A","M","M"],
   ["O","M","A","F","P","A","L","A","A"],
-  ["P","A","L","A", "V","R","A","S"],
   ["O","M","A","F","P","A","L","A","A"],
+  ["P","A","L","A", "V","R","A","S"],
+  ["A","M","O","R","R","O","M","A","F"],
   ["A","M","O","R","R","O","M","A","F"],
   ["O","M","A","F","P","A","L","A","A"]
 
@@ -13,37 +14,27 @@ var board =
 
 var data = "AMOR ALEGRIA PALAVRAS NOITE CADEIRA PROGRAMAÇÃO";
 
-var readBoard = function(board, data) {
+function Search(board, data) {
 
+  this.readBoardLineRight = function(){
     for(line in board) {
       let word = ""
-      console.log("---------- outra linha ----------")
-      for(let eachLetter = 0; eachLetter < board[line].length; eachLetter++) {
 
+      for(let eachLetter = 0; eachLetter < board[line].length; eachLetter++) {
         word += board[line][eachLetter]
 
         if(word.length >= 3) {
-
-          var result = data.match(word)
-
-          if(result == null ){
-            console.log("OK EU NÃO TENHO UMA PALAVRA MAS VOU CONTINUAR BUSCANDO")
-          }
-
-          console.log('preste atenção aqui', data.search(word + " "))
-
-          if (data.search(word + " ")   === -1) {
-            console.log("OK EU NÃO TENHO UMA PALAVRA MAS VOU CONTINUAR BUSCANDO")
-          } else {
+          if (data.search(word + " ") != -1) {
             return word
           }
         }
       }
     }
   }
+}
 
-console.log(readBoard(board, data))
+var search = new Search(board, data)
+console.log(search.readBoardLineRight(board, data))
 
-          //
-          // console.log("palavra até aqgora", word)
-          // console.log("resultado da paravra de 3 letras", result)
+
+module.exports = { Search }

cf8a1a9dad1f2aa41bdb90cd45073cc55b46526b
NicolsNeto <npneto@thoughtworks.com>
2018-11-08 11:59:53 -0200

    buscando  uma letra de cada vez, mas preciso parar na palavra

--- Search/search.js
+++ Search/search.js
@@ -2,26 +2,36 @@
 var board =
 [
   ["A","M","O","R","R","O","M","A"],
+  ["A","L","E","G","R","I","A","M"],
   ["A","L","E","G","R","I","A","M"]
 ];
 
-var data = "Amor Alegria Palavras Noite Cadeira Desistir Programação"
+var data = "AMOR ALEGRIA PALAVRAS NOITE CADEIRA PROGRAMAÇÃO";
 
-var readBoard = function(board) {
+var readBoard = function(board, data) {
 
   let word = ""
 
     for(line in board) {
 
-      for(let eachLetter = 0; eachLetter < board[line].lenght; eachLetter++) {
+      for(let eachLetter = 0; eachLetter < board[line].length; eachLetter++) {
+
         word += board[line][eachLetter]
 
-        if(word.lenght >= 3) {
+        if(word.length >= 3) {
+
           var result = data.match(word)
+
+          console.log("palavra até aqgora", word)
+          console.log("resultado da paravra de 3 letras", result)
+
+          if(result == null ){
+            return "não tem uma palavra nesta linha "
+          }
         }
       }
     }
     return word
 }
 
-console.log(readBoard(board))
+console.log(readBoard(board, data))

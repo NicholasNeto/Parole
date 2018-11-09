b27545d361d8d1bf8b2ff3d7d6a131c76354aa77
NicolsNeto <npneto@thoughtworks.com>
2018-11-09 15:31:38 -0200

    readBoardLineLeft

--- Search/search.js
+++ Search/search.js
@@ -5,10 +5,12 @@ var board =
   ["A","V","E","G","R","I","A","M","M"],
   ["O","M","A","F","P","A","L","A","A"],
   ["O","M","A","F","P","A","L","A","A"],
-  ["P","A","L","A", "V","R","A","S"],
+  ["A","V","E","G","R","R","O","M","A"],
+  ["P","A","L","A", "V","R","A","S","Z"],
   ["A","M","O","R","R","O","M","A","F"],
   ["A","M","O","R","R","O","M","A","F"],
   ["O","M","A","F","P","A","L","A","A"]
+
 
 ];
 
@@ -31,10 +33,27 @@ function Search(board, data) {
       }
     }
   }
+
+  this.readBoardLineLeft = function(board, data) {
+
+    for(line in board) {
+      let word = ""
+
+      for(let eachLetter = board[line].length - 1; eachLetter > 0; eachLetter--) {
+        word += board[line][eachLetter]
+
+        if(word.length >= 3) {
+          if (data.search(word + " ") != -1) {
+            return word
+          }
+        }
+      }
+    }
+  }
 }
 
 var search = new Search(board, data)
 console.log(search.readBoardLineRight(board, data))
-
+console.log(search.readBoardLineLeft(board, data))
 
 module.exports = { Search }
